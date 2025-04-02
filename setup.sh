4a2d5670ab312119afc72d43e4e5745761828510
Gareth Becker <beckergareth38@gmail.com>
2025-04-02 18:48:20 +0200

    rclone

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+###########################
+## INSTALLING UTILITIES ##
+##########################
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
 ##################################
 ## SETTING UP FOLDER STRUCTURE ##
