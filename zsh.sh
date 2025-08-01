c7982b2491a60ba8a5f2497dcb64e4da60f9a9bf
Gareth Becker <beckergareth38@gmail.com>
2025-08-01 16:22:54 +0200

    Fix zsh

--- zsh.sh
+++ zsh.sh
@@ -1,14 +1,11 @@
 sudo apt -y install zsh
 
-zsh
-
-rm ~/.zshrc
-cp ./configs/zshrc ~/.zshrc
-cp ./configs/my_profile ~/.my_profile
-source ~/.zshrc
-
 
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+rm ~/.zshrc
+cp ./configs/zshrc ~/.zshrc
+cp ./configs/my_profile ~/.my_profile
+source ~/.zshrc
