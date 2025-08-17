f9a2a3aa8b494362110d5db2e3f6e4fc035c929e
Gareth Becker <beckergareth38@gmail.com>
2025-08-17 12:00:04 +0200

    asdf

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "INSTALLING REQUIRED LIBRARIES"
 sudo apt update; sudo apt install -y build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev curl git \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-libpq-dev python2-dev
+libpq-dev python3-dev diodon
 
 ###########################
 ## INSTALLING UTILITIES ##
@@ -80,7 +80,7 @@ git config --global user.email "beckergareth38@gmail.com"
 ##################
 ## Setup docker ##
 ##################
-bash docker.sh 
+
 
 ###############
 ## Setup ZSH ##
